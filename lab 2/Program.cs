84738c2ad8a5026809d725831ce2cc14946fff2d
Владимир Молчанов <pythonsonnn@gmail.com>
2025-03-18 17:51:02 +0300

    message

--- lab 2/Program.cs
+++ lab 2/Program.cs
@@ -7,36 +7,96 @@ namespace lab2
     {
         static void Main()
         {
-            Student first = new("Anna","ddd",20,2005);
-            Student second = new("Борис","ddd",20,2005);
+            StudentGroup group = new StudentGroup();
 
-            first.Print();
-            second.Print();
+            Student first = new("Anna","ddd",20,2005,1);
+            Student second = new("Борис","ddd",20,2005,2);
+
+            group.Add(first);
+            group.Add(second);
+
+            while (true)
+            {
+                Console.WriteLine("Введите команду (1 - Добавление,  2 - Список всех студентов, 3 - Выход):");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Student newStudent = Student.ProcessInput();
+                        if (newStudent != null)
+                        {
+                            group.Add(newStudent);
+                            Console.WriteLine("Студент добавлен.");
+                        }
+                        break;
+                    case "2":
+                        group.PrintAllStudents();
+                        break;
+                    case "3":
+                        return;
+                    default:
+                        Console.WriteLine("Неизвестная команда");
+                        break;
+                }
+            }
         }
-    }
+    }   
     class Student
     {
         public string name;
         public string family;
         public int age;
         public int date;
-        public Student(string name, string family, int age, int date)
+        public int ID;
+        public Student(string name, string family, int age, int date, int ID)
+        // конструктор для инициализации параметров объекта
         {
             this.name = name;
             this.family = family;
             this.age = age;
             this.date = date;
+            this.ID = ID;
         } 
-        
-        input1 = Console.ReadLine()
-        switch (input1)
+        public static Student ProcessInput()
         {
-            case 1:
-            Console.WriteLine("Довавление");
-            break;
+            Console.WriteLine("Введите Имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите фамилию:");
+            string family = Console.ReadLine();
+            Console.WriteLine("Введите возраст:");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите год рождения:");
+            int date = int.Parse(Console.ReadLine());
+            int ID = 3;
+
+            return new Student(name, family, age, date, ID);
         }
-        public void Print() => Console.WriteLine($"Имя: {name},Фамилия: {family},Возраст: {age},Дата рождения {date}");
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {name}, Фамилия: {family}, Возраст: {age}, Дата рождения: {date}, id {ID}");
+        }
     }
-    
-    
+    class StudentGroup
+    {
+        public List<Student> Students { get; } = new List<Student>();
+        public void Add(Student student)
+        {
+            Students.Add(student);
+        }
+        public void PrintAllStudents()
+        {
+            if (Students.Count == 0)
+            {
+                Console.WriteLine("Список студентов пуст.");
+            }
+            else
+            {
+                foreach (var student in Students)
+                {
+                    student.Print();
+                }
+            }
+        }
+    }
 }
--- lab 2/obj/Debug/net9.0/lab 2.AssemblyInfo.cs
+++ lab 2/obj/Debug/net9.0/lab 2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60a1a588b4600823c338b1554ad4a86a50e13ba7")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
