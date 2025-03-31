861a17987a2c69a58bed735dfc13635b21e9baf7
Владимир Молчанов <pythonsonnn@gmail.com>
2025-03-31 17:47:35 +0300

    essage

---
+++ lab 4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab4{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+    class Publication
+    {
+        string name;
+        double prise;
+        public Publication(string name,double prise)
+        {
+            this.name = name;
+            this.prise = prise;
+        }
+        public void Getdata()
+        {
+
+        }
+        public void Putdata()
+        {
+
+        } 
+    }
+    class Book
+    {
+        int NumberOfPage;
+        
+        public Book(int NumberOfPage)
+        {
+            this.NumberOfPage = NumberOfPage;
+        }
+    }
+    class Type
+    {
+        int RecordingTime;//в минутах
+        public Type(int RecordingTime)
+        {
+            this.RecordingTime = RecordingTime;
+        }
+    }
+}   
+
+
