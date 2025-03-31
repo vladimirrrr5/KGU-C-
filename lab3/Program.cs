92bcbe18e11466a37ff60aeed5751827b1ceb11c
Владимир Молчанов <pythonsonnn@gmail.com>
2025-03-31 17:04:25 +0300

    essage

---
+++ lab3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace lab3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Add adder = new Add();
+            adder.Addition(5,2);
+            adder.Addition(5.2f,3.2f);
+            adder.Addition("Иван", "Иванов");
+        }
+    }
+    class Add
+    {
+        public void Addition(int a, int b)
+        {
+            int res = a + b;
+            Console.WriteLine(res);
+        }
+        public void Addition(float a, float b)
+        {
+            float res = a + b;
+            Console.WriteLine(res);
+        }
+        public void Addition(string a, string b)
+        {
+            string res = a + b;
+            Console.WriteLine(res);
+        }
+    }
+}
